8e6f8c10faa0c4afb59d24d265a4c8e8d0799b83
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-25 21:00:15 +0700

    chore : setup eslint prettier config and plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
     'airbnb/hooks',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
