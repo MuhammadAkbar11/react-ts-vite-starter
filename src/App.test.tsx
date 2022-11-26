7e37245cb98d273132cc80660edb6d2508194209
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-26 18:45:34 +0700

    test: App test

---
+++ src/App.test.tsx
@@ -0,0 +1,18 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  it('Render Title', () => {
+    // ARRANGE
+    // ACT
+    render(<App />);
+
+    // EXPECT
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Vite, React, Typescript and TailwindCss Starter');
+  });
+});
