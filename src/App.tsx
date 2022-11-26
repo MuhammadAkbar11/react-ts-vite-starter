71e8ba9c386c514cdd9e53692c5fdc659008e5c0
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-26 22:57:35 +0700

    feat: Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 export function App() {
   return (
-    <div className="box">
-      <h1>Vite, React, Typescript and TailwindCss Starter</h1>
-      <Routes>
-        <Route path="/" />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Props = {};
+
+// eslint-disable-next-line no-empty-pattern
+function Home({}: Props) {
+  return (
+    <div className="box">
+      <h1>Vite, React, Typescript and TailwindCss Starter</h1>
+    </div>
+  );
+}
+
+export default Home;
